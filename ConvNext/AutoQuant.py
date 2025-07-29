244854e3387df93154280d25cbb1ffd41d2732a5
rayari-1729 <109236841+rayari-1729@users.noreply.github.com>
2025-07-30 00:26:32 +0530

    Add files via upload

---
+++ ConvNext/AutoQuant.py
@@ -0,0 +1,394 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import argparse
+from asyncio import base_tasks
+import datetime
+from unittest import result
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import json
+import os
+
+from pathlib import Path
+
+from timm.data.mixup import Mixup
+from timm.models import create_model
+from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+from timm.utils import ModelEma
+from optim_factory import create_optimizer, LayerDecayValueAssigner
+
+from datasets import build_dataset, build_dataset_cal
+from engine import train_one_epoch, evaluate
+
+from utils import NativeScalerWithGradNormCount as NativeScaler
+import utils
+import models.convnext
+import models.convnext_isotropic
+
+from torchvision import datasets, transforms
+from datasets import build_transform
+
+def str2bool(v):
+    """
+    Converts string to bool type; enables command line 
+    arguments in the format of '--arg1 true --arg2 false'
+    """
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('ConvNeXt training and evaluation script for image classification', add_help=False)
+    parser.add_argument('--batch_size', default=64, type=int,
+                        help='Per GPU batch size')
+    parser.add_argument('--epochs', default=300, type=int)
+    parser.add_argument('--update_freq', default=1, type=int,
+                        help='gradient accumulation steps')
+
+    # Model parameters
+    parser.add_argument('--model', default='convnext_tiny', type=str, metavar='MODEL',
+                        help='Name of model to train')
+    parser.add_argument('--drop_path', type=float, default=0, metavar='PCT',
+                        help='Drop path rate (default: 0.0)')
+    parser.add_argument('--input_size', default=224, type=int,
+                        help='image input size')
+    parser.add_argument('--layer_scale_init_value', default=1e-6, type=float,
+                        help="Layer scale initial values")
+
+    # EMA related parameters
+    parser.add_argument('--model_ema', type=str2bool, default=False)
+    parser.add_argument('--model_ema_decay', type=float, default=0.9999, help='')
+    parser.add_argument('--model_ema_force_cpu', type=str2bool, default=False, help='')
+    parser.add_argument('--model_ema_eval', type=str2bool, default=False, help='Using ema to eval during training.')
+
+    # Optimization parameters
+    parser.add_argument('--opt', default='adamw', type=str, metavar='OPTIMIZER',
+                        help='Optimizer (default: "adamw"')
+    parser.add_argument('--opt_eps', default=1e-8, type=float, metavar='EPSILON',
+                        help='Optimizer Epsilon (default: 1e-8)')
+    parser.add_argument('--opt_betas', default=None, type=float, nargs='+', metavar='BETA',
+                        help='Optimizer Betas (default: None, use opt default)')
+    parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM',
+                        help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+    parser.add_argument('--weight_decay_end', type=float, default=None, help="""Final value of the
+        weight decay. We use a cosine schedule for WD and using a larger decay by
+        the end of training improves performance for ViTs.""")
+
+    parser.add_argument('--lr', type=float, default=4e-3, metavar='LR',
+                        help='learning rate (default: 4e-3), with total batch size 4096')
+    parser.add_argument('--layer_decay', type=float, default=1.0)
+    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0 (1e-6)')
+    parser.add_argument('--warmup_epochs', type=int, default=20, metavar='N',
+                        help='epochs to warmup LR, if scheduler supports')
+    parser.add_argument('--warmup_steps', type=int, default=-1, metavar='N',
+                        help='num of steps to warmup LR, will overload warmup_epochs if set > 0')
+
+    # Augmentation parameters
+    parser.add_argument('--color_jitter', type=float, default=0.4, metavar='PCT',
+                        help='Color jitter factor (default: 0.4)')
+    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
+                        help='Use AutoAugment policy. "v0" or "original". " + "(default: rand-m9-mstd0.5-inc1)'),
+    parser.add_argument('--smoothing', type=float, default=0.1,
+                        help='Label smoothing (default: 0.1)')
+    parser.add_argument('--train_interpolation', type=str, default='bicubic',
+                        help='Training interpolation (random, bilinear, bicubic default: "bicubic")')
+
+    # Evaluation parameters
+    parser.add_argument('--crop_pct', type=float, default=None)
+
+    # * Random Erase params
+    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
+                        help='Random erase prob (default: 0.25)')
+    parser.add_argument('--remode', type=str, default='pixel',
+                        help='Random erase mode (default: "pixel")')
+    parser.add_argument('--recount', type=int, default=1,
+                        help='Random erase count (default: 1)')
+    parser.add_argument('--resplit', type=str2bool, default=False,
+                        help='Do not random erase first (clean) augmentation split')
+
+    # * Mixup params
+    parser.add_argument('--mixup', type=float, default=0.8,
+                        help='mixup alpha, mixup enabled if > 0.')
+    parser.add_argument('--cutmix', type=float, default=1.0,
+                        help='cutmix alpha, cutmix enabled if > 0.')
+    parser.add_argument('--cutmix_minmax', type=float, nargs='+', default=None,
+                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
+    parser.add_argument('--mixup_prob', type=float, default=1.0,
+                        help='Probability of performing mixup or cutmix when either/both is enabled')
+    parser.add_argument('--mixup_switch_prob', type=float, default=0.5,
+                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    parser.add_argument('--mixup_mode', type=str, default='batch',
+                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
+
+    # * Finetuning params
+    parser.add_argument('--finetune', default='',
+                        help='finetune from checkpoint')
+    parser.add_argument('--head_init_scale', default=1.0, type=float,
+                        help='classifier head initial scale, typically adjusted in fine-tuning')
+    parser.add_argument('--model_key', default='model|module', type=str,
+                        help='which key to load from saved state dict, usually model or model_ema')
+    parser.add_argument('--model_prefix', default='', type=str)
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
+                        help='dataset path')
+    parser.add_argument('--eval_data_path', default=None, type=str,
+                        help='dataset path for evaluation')
+    parser.add_argument('--nb_classes', default=1000, type=int,
+                        help='number of the classification types')
+    parser.add_argument('--imagenet_default_mean_and_std', type=str2bool, default=True)
+    parser.add_argument('--data_set', default='IMNET', choices=['CIFAR', 'IMNET', 'image_folder'],
+                        type=str, help='ImageNet dataset path')
+    parser.add_argument('--output_dir', default='',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default=None,
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+    parser.add_argument('--auto_resume', type=str2bool, default=True)
+    parser.add_argument('--save_ckpt', type=str2bool, default=True)
+    parser.add_argument('--save_ckpt_freq', default=1, type=int)
+    parser.add_argument('--save_ckpt_num', default=3, type=int)
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--eval', type=str2bool, default=False,
+                        help='Perform evaluation only')
+    parser.add_argument('--dist_eval', type=str2bool, default=True,
+                        help='Enabling distributed evaluation')
+    parser.add_argument('--disable_eval', type=str2bool, default=False,
+                        help='Disabling evaluation during training')
+    parser.add_argument('--num_workers', default=1, type=int)
+    parser.add_argument('--pin_mem', type=str2bool, default=True,
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', type=str2bool, default=False)
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    parser.add_argument('--use_amp', type=str2bool, default=False, 
+                        help="Use PyTorch's AMP (Automatic Mixed Precision) or not")
+
+    # Weights and Biases arguments
+    parser.add_argument('--enable_wandb', type=str2bool, default=False,
+                        help="enable logging to Weights and Biases")
+    parser.add_argument('--project', default='convnext', type=str,
+                        help="The name of the W&B project where you're sending the new run.")
+    parser.add_argument('--wandb_ckpt', type=str2bool, default=False,
+                        help="Save model checkpoints as W&B Artifacts.")
+
+    return parser
+
+def main(args):
+    utils.init_distributed_mode(args)
+    print(args)
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + utils.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    cudnn.benchmark = True
+
+    dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
+    if args.disable_eval:
+        args.dist_eval = False
+        dataset_val = None
+    else:
+        dataset_val, _ = build_dataset(is_train=False, args=args)
+
+    num_tasks = utils.get_world_size()
+    global_rank = utils.get_rank()
+
+    sampler_train = torch.utils.data.DistributedSampler(
+        dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True, seed=args.seed,
+    )
+    print("Sampler_train = %s" % str(sampler_train))
+    if args.dist_eval:
+        if len(dataset_val) % num_tasks != 0:
+            print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
+                    'This will slightly alter validation results as extra duplicate entries are added to achieve '
+                    'equal num of samples per-process.')
+        sampler_val = torch.utils.data.DistributedSampler(
+            dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=False)
+    
+    else:
+        sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+        
+
+    if dataset_val is not None:
+        data_loader_val = torch.utils.data.DataLoader(
+            dataset_val, sampler=sampler_val,
+            batch_size=int(1.5 * args.batch_size),
+            # batch_size = 128,
+            num_workers=args.num_workers,
+            pin_memory=args.pin_mem,
+            drop_last=False
+        )
+
+    else:
+        data_loader_val = None
+
+
+    mixup_fn = None
+    mixup_active = args.mixup > 0 or args.cutmix > 0. or args.cutmix_minmax is not None
+    if mixup_active:
+        print("Mixup is activated!")
+        mixup_fn = Mixup(
+            mixup_alpha=args.mixup, cutmix_alpha=args.cutmix, cutmix_minmax=args.cutmix_minmax,
+            prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
+            label_smoothing=args.smoothing, num_classes=args.nb_classes)
+
+    model = create_model(
+        args.model, 
+        pretrained=False, 
+        num_classes=args.nb_classes, 
+        drop_path_rate=args.drop_path,
+        layer_scale_init_value=args.layer_scale_init_value,
+        head_init_scale=args.head_init_scale,
+        )
+
+    if args.finetune:
+        if args.finetune.startswith('https'):
+            checkpoint = torch.hub.load_state_dict_from_url(
+                args.finetune, map_location='cpu', check_hash=True)
+        else:
+            checkpoint = torch.load(args.finetune, map_location='cpu')
+
+        print("Load ckpt from %s" % args.finetune)
+        checkpoint_model = None
+        for model_key in args.model_key.split('|'):
+            if model_key in checkpoint:
+                checkpoint_model = checkpoint[model_key]
+                print("Load state_dict by model_key = %s" % model_key)
+                break
+        if checkpoint_model is None:
+            checkpoint_model = checkpoint
+        state_dict = model.state_dict()
+        for k in ['head.weight', 'head.bias']:
+            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint_model[k]
+        utils.load_state_dict(model, checkpoint_model, prefix=args.model_prefix)
+    model.to(device)
+
+
+    # total_batch_size = args.batch_size * args.update_freq * utils.get_world_size()
+    total_batch_size = 128
+
+    print("Batch size = %d" % total_batch_size)
+    print("Update frequent = %d" % args.update_freq)
+
+    utils.auto_load_model_no_optimizer(
+        args=args, model=model)
+    model.eval()
+    model.cuda()
+
+    dummy_input = torch.randn(1, 3, 224, 224 ).cuda()
+    #torch.onnx.export(model, dummy_input,  "ConvexNet-B.onnx",opset_version=11)
+    import onnxruntime as ort
+    #model = ort.InferenceSession("/local/workspace/rayari/AIMET_Tools/Models/ConvNeXt/ConvexNet-B.onnx")
+
+    print("Passing model to JIT Trace")
+    traced_model = torch.jit.trace(model,dummy_input)
+    print("------------------------------------")
+    
+    print("\nPassing model to Model Validator")
+    from aimet_torch.model_validator.model_validator import ModelValidator
+    ModelValidator.validate_model(model, model_input=dummy_input)
+    print("------------------------------------")
+
+
+    print("Passing model to prepare_model api")
+    from aimet_torch.model_preparer import prepare_model
+    model = prepare_model(model)
+    print("------------------------------------")
+
+    print("\nPassing model to Model Validator")
+    from aimet_torch.model_validator.model_validator import ModelValidator
+    ModelValidator.validate_model(model, model_input=dummy_input)
+    print("---------------- **************************** --------------------")
+
+    print("\n==================>Model Validation Before Passing into AutoQuant=============>")
+    validation = evaluate(model, data_loader_val)
+    print(f"Accuracy of the network on {len(dataset_val)} test images: {validation['acc1']:.5f}%")
+
+    exit()
+    
+    print("Passing model to AutoQuant")
+
+    dataset_cal = build_dataset_cal(is_train=False, args=args)
+    data_loader_cal = torch.utils.data.DataLoader(
+            dataset_cal, #sampler=sampler_val,
+            # batch_size=int(1.5 * args.batch_size),
+            batch_size=1,
+            num_workers=args.num_workers,
+            pin_memory=args.pin_mem,
+            drop_last=False
+        )
+
+    def eval_func(model, iter):
+        print("<==================Started Evaluation==============>")
+        v = evaluate(model, data_loader_val)
+        return v['acc1']
+
+    from aimet_torch.pro.auto_quant import AutoQuant
+
+    auto_quant = AutoQuant(allowed_accuracy_drop=0.005,
+                   unlabeled_dataset_iterable=data_loader_cal,
+                   eval_callback=eval_func, 
+                   default_quant_scheme='tf',
+                   default_config_file = "/media/DATA2/rayari/ConvNeXt/htp_quantsim_config_pcq.json")
+
+    from aimet_torch.adaround.adaround_weight import AdaroundParameters
+    adaround_params = AdaroundParameters(data_loader_cal, num_batches=1)
+    auto_quant.set_adaround_params(adaround_params)
+
+    from aimet_torch.tensor_quantizer import QuantizationDataType
+    
+    auto_quant_model, accuracy, encoding_path, pareto_front=\
+                auto_quant.apply(model,
+                 dummy_input_on_cpu=dummy_input.cpu(),
+                 dummy_input_on_gpu=dummy_input.cuda(),
+                 results_dir="/media/DATA2/rayari/ConvNeXt/AutoQuant/v1.22pro_ConvNext_TF_PCQ_2kCal_MV_bs96/")
+
+    print("------------------------------------")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('ConvNeXt training and evaluation script', parents=[get_args_parser()])
+    args = parser.parse_args()
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
+
+
+    #python main.py --model convnext_base --eval true --resume https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_1k_224.pth --input_size 224 --drop_path 0.2 --data_path /local/mnt/workspace/selventh/Datasets/Imagenet/
